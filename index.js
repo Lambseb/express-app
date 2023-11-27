68a5434a7da4f9478cf33c44ba5942b3b373b1ae
Sébastien LAMBRANCA <116831255+Lambseb@users.noreply.github.com>
2023-11-27 12:19:57 +0100

    make test jest

--- index.js
+++ index.js
@@ -1,53 +1,11 @@
-const express = require("express");
-const app = express();
+const app = require("./app");
+
 const port = 5000;
 
-app.listen(port, (err) => {
-  if (err) {
-    console.error("Something bad happened");
-  } else {
-    console.log(`Server is listening on ${port}`);
-  }
-});
-
-const movies = [
-  {
-    id: 1,
-    title: "Citizen Kane",
-    director: "Orson Wells",
-    year: "1941",
-    colors: false,
-    duration: 120,
-  },
-  {
-    id: 2,
-    title: "The Godfather",
-    director: "Francis Ford Coppola",
-    year: "1972",
-    colors: true,
-    duration: 180,
-  },
-  {
-    id: 3,
-    title: "Pulp Fiction",
-    director: "Quentin Tarantino",
-    year: "1994",
-    color: true,
-    duration: 180,
-  },
-];
-
-app.get("/", (req, res) => {
-  res.send("Welcome my favourite movie list");
-});
-
-app.get("/app/movies", (req, res) => {
-  res.json(movies).sendStatus(200);
-});
-
-const filtered = (req, res) => {
-  res
-    .status(404)
-    .send(movies.find((found) => found.id === parseInt(req.params.id)));
-};
-app.get("/app/movies/:id", filtered);
+app
+  .listen(port, () => {
+    console.info(`Server is listening on port ${port}`);
+  })
+  .on("error", (err) => {
+    console.error("Error:", err.message);
+  });
---
+++ tests/movies.test.js
@@ -0,0 +1,19 @@
+const { intersects } = require("semver");
+const request = require("supertest");
+const app = require("../app");
+describe("GET /api/movies", () => {
+  it("should return all movies", async () => {
+    const response = await request(app).get("/api/movies");
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.status).toEqual(200);
+  });
+  it("should return movie names", async () => {
+    const response = await request(app).get("/api/movies/1");
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.status).toEqual(200);
+  });
+  it("should return 404 error", async () => {
+    const response = await request(app).get("/api/movies/0");
+    expect(response.status).toEqual(404);
+  });
+});
